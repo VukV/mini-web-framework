7bbed54af4197377c53c219542a9e5c1900d9b37
VukV <vuk.vukovic.2000@gmail.com>
2022-11-03 12:29:10 +0100

    finished scanning all classes within the project, started working on dependency injection engine

--- src/main/java/framework/engine/DependencyInjectionEngine.java
+++ src/main/java/framework/engine/DependencyInjectionEngine.java
@@ -1,4 +1,48 @@
 package framework.engine;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 public class DependencyInjectionEngine {
+
+
+    /**
+     * DependencyContainer keeps information about interface implementations.
+     * String - unique qualifier name
+     * Class - interface implementation
+     */
+    private Map<String, Class<?>> dependencyContainer;
+    private Set<Class<?>> services;
+    private Set<Class<?>> containers;
+
+    private ClassScanner classScanner;
+
+    public DependencyInjectionEngine() {
+        dependencyContainer = new HashMap<>();
+        services = new HashSet<>();
+        containers = new HashSet<>();
+        classScanner = new ClassScanner();
+
+        arrangeClasses();
+        injectFields();
+    }
+
+    private void arrangeClasses(){
+        //TODO
+    }
+
+    private void injectFields(){
+        //TODO
+    }
+
+    private void printVerbose(Field field, Object objectInstance){
+        System.out.println("Initialized " + field.getType().getName() + " " + field.getName() + " in "
+                + field.getDeclaringClass().getName() + " on "
+                + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy"))
+                + " with " + objectInstance.hashCode());
+    }
 }
